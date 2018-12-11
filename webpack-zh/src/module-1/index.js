aed9a1963116798bd60e73ddf5ee2347a22d6fe7
zzh49358 <zhangzhihao@chebada.com>
2018-12-11 17:42:01 +0800

    long time cache

--- webpack-zh/src/module-1/index.js
+++ webpack-zh/src/module-1/index.js
@@ -2,6 +2,7 @@ import { sample } from '../common'
 
 export function logger(msgs) {
   console.log('module1')
+  console.log('11111');
   const msg = sample(msgs)
   console.info(msg)
 }
--- webpack-zh/webpack.config.js
+++ webpack-zh/webpack.config.js
@@ -8,10 +8,47 @@ module.exports = {
 
   output: {
     filename: '[name].[chunkhash:8].js',
+    chunkFilename: '[name].[chunkhash:8].js'
   },
 
   optimization: {
     runtimeChunk: true,
-    splitChunks: {},
+    moduleIds: 'hashed',
+    splitChunks: {
+        chunks: "all",
+        minSize: 30000,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        automaticNameDelimiter: '~',
+        name: true,
+        cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "initial" // 只打包初始时依赖的第三方
+        },
+        commons: {
+          minSize: 0,
+          test: require.resolve("./src/common/index.js"), // 可自定义拓展你的规则
+          minChunks: 1, // 最小共用次数
+          enforce: true,
+          reuseExistingChunk: true
+      },
+         module1: {
+             test: require.resolve("./src/module-1/index.js"), // 可自定义拓展你的规则
+             minChunks: 1, // 最小共用次数
+             minSize: 0,
+             enforce: true,
+             reuseExistingChunk: true
+         },
+         module2: {
+             test: require.resolve("./src/module-2/index.js"), // 可自定义拓展你的规则
+             minChunks: 1, // 最小共用次数
+             minSize: 0,
+             enforce: true,
+             reuseExistingChunk: true
+         },
+      }
+    }
   },
 }
